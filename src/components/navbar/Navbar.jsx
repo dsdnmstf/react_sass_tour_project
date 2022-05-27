3d69e8da1e838c22021e2921446d241afb16f02b
musrafa <dsdnmstf@gmail.com>
2022-05-27 19:50:31 +0200

    header added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,15 @@ import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <a href="/">ABOUT US</a>
+      <a href="/">FOR YOU</a>
+      <a href="/">SERVICES</a>
+      <a href="/">BLOG</a>
+      <a href="/">VLOG</a>
+      <a href="/">CONTACT</a>
+    </div>
+  );
 }
 
 export default Navbar
